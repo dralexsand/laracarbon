53dbf34567174e89d4b3ad249b557ec0a2dd80b2
dralexsand <dr.alex.sand.111@gmail.com>
2023-03-21 19:04:50 +0600

    Add job after complete

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+php artisan cache:clear
+php artisan view:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:cache
+php artisan config:cache
+php artisan route:cache
+php artisan optimize
+composer dump-autoload
